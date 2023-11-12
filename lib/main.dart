6257564543c9f3c4d10d1c085db93e9828af5cb3
Vickyvirus <vickyhilako11@gmail.com>
2023-11-12 22:33:31 +0530

    statful

--- lib/main.dart
+++ lib/main.dart
@@ -6,62 +6,54 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 245, 53, 5)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Images'),
+      theme: ThemeData(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyWidgetState extends State<MyHomePage> {
+  var count = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: InkWell(
-            onTap: () {
-              print("Click taped on Container");
-            },
-            onDoubleTap: () {
-              print("Click Double taped on Container");
-            },
-            child: Container(
-              width: 200,
-              height: 200,
-              color: Colors.blueGrey,
-              child: Center(
-                child: InkWell(
-                  onTap: () {
-                    print("Text Widget Tapped");
-                  },
-                  child: Text(
-                    "click here",
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
+      appBar: AppBar(
+        title: Text("stateful"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Count:$count",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
-          ),
-        ));
+            ElevatedButton(
+                onPressed: () {
+                  // count++;
+                  // print(count);
+
+                  setState(() {
+                    count++;
+                    print(count);
+                  });
+                },
+                child: Text("Increement Count"))
+          ],
+        ),
+      ),
+    );
   }
 }
